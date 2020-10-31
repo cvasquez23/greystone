570b795a07cd47692ee1f435b15e780c2d5a7f79
cvasquez23 <chris@chrisvasquez.dev>
2020-10-31 04:40:49 -0400

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
-// const rename = require('gulp-rename');
+const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
+const purgecss = require('gulp-purgecss');
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+const runSequence = require('run-sequence');
 
 // compile scss into css
 function style() {
@@ -30,6 +34,66 @@ function watch() {
   gulp.watch('./**/*.php').on('change', browserSync.reload);
   gulp.watch('./js/.**/*.js').on('change', browserSync.reload);
 }
+
+gulp.task('purgecss', () => {
+  return gulp
+    .src('./css/*.css')
+    .pipe(
+      purgecss({
+        content: ['./*.html'],
+      })
+    )
+    .pipe(gulp.dest('./css/'));
+});
+
+// minify css
+gulp.task('styles', () => {
+  return (
+    gulp
+      .src('./scss/**/*.scss')
+
+      // autoprefixer
+      .pipe(autoprefixer())
+
+      // pass through scss compiler AND minify
+      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+
+      // where to save
+      .pipe(gulp.dest('./css'))
+  );
+});
+
+// minify JS
+gulp.task('scripts', () => {
+  return (
+    gulp
+      .src('./js/*.js')
+      // minify the files
+      .pipe(uglify())
+      // where to save
+      .pipe(gulp.dest('./js'))
+  );
+});
+
+// minify HTML
+gulp.task('pages', function () {
+  return gulp
+    .src(['./**/*.html'])
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+      })
+    )
+    .pipe(gulp.dest('./'));
+});
+
+// Minify JS, HTML, and CSS
+gulp.task('minify', function () {
+  runSequence('styles', 'scripts', 'pages');
+});
+
+gulp.task('clean', () => del(['./']));
 
 exports.style = style;
 exports.watch = watch;
